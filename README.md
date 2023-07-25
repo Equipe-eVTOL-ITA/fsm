0e20236b26c21ab8ca2b4244f6f61fb53a7ab815
moisesmgft <moisesmgft@gmail.com>
2023-07-25 13:19:53 -0300

    created fsm package

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# fsm
+# Final State Machine
 Implementação de máquina de estados finita.
---
+++ src/fsm.cpp
@@ -0,0 +1,49 @@
+#include "fsm/fsm.hpp"
+
+#include<iostream>
+
+FSM::FSM(State* initialState) : currentState{initialState} {}
+
+/**
+ * @brief Get the current state of the FSM.
+ */
+State* FSM::returnState()
+{
+    return this->currentState;
+}
+
+/**
+ * @brief Execute act() function from current state.
+ */
+void FSM::executeState()
+{
+    this->currentState->act();
+
+    return;
+}
+
+/**
+ * @brief Check if a condition for transition is true. If it is, the current state is changed.
+ */
+void FSM::changeState()
+{
+    for(auto i : this->currentState->transitions)
+    {
+        if(i.first())
+        {
+            this->currentState = i.second;
+            break;
+        }
+    }
+
+    return;
+}
+
+/**
+ * @brief Call this function in the loop for checking transitions and executing actions.
+ */
+void FSM::executeFSM()
+{
+    this->changeState();
+    this->executeState();
+}
